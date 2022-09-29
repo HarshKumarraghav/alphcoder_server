1dccb9023000f1c6c45d71154927a572dc44b341
Harsh Raghav <harshraghav@Harshs-MacBook-Air.local>
2022-09-29 22:40:11 +0530

    Alphacoder: database updated 💾

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -39,6 +39,7 @@ func DBinstance() *mongo.Client {
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = client.Database("alphacoder_databse").Collection(collectionName)
+
+	var collection *mongo.Collection = client.Database("alphacoder_database").Collection(collectionName)
 	return collection
 }
