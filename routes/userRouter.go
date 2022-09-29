1f0f479a1e6125f9eb0a6f8c1a96897dc24faf3d
Harsh Raghav <harshraghav@Harshs-MacBook-Air.local>
2022-09-30 04:24:33 +0530

    Alphacoder: routes path edits 🚏

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -11,7 +11,7 @@ func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/users", controller.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controller.GetUser())
 	incomingRoutes.GET("/sheets/frazsheet", controller.Frazsheet())
-	incomingRoutes.GET("/sheets/sheet450", controller.Sheet450())
+	incomingRoutes.GET("/sheets/dsa450sheet", controller.Sheet450())
 	incomingRoutes.GET("/sheets/apnasheet", controller.ApnacollegeSheet())
-	incomingRoutes.GET("/sheets/sheet75", controller.SheetBlind75())
+	incomingRoutes.GET("/sheets/bind75sheet", controller.SheetBlind75())
 }
