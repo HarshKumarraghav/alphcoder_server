695e99899ce296b743139d15c17bcf2e02d8a644
Harsh Raghav <harshraghav@Harshs-MacBook-Air.local>
2022-09-30 07:47:11 +0530

    .ENV: delete some enviroment variables ␡

--- main.go
+++ main.go
@@ -31,5 +31,6 @@ func main() {
 	router.GET("/api-2", func(c *gin.Context) {
 		c.JSON(200, gin.H{"success": "access granted for api-2"})
 	})
+	gin.SetMode(gin.ReleaseMode)
 	router.Run(":" + port)
 }
