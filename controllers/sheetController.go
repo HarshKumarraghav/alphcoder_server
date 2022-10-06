0328cb9de72cba155de310855f16b57d0aeac76b
Harsh Raghav <harshraghav@Harshs-MacBook-Air.local>
2022-10-06 11:42:10 +0530

    Alphacoder: Record per page increased

--- controllers/sheetController.go
+++ controllers/sheetController.go
@@ -29,7 +29,7 @@ func AllQuesion() gin.HandlerFunc {
 
 		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
 		if err != nil || recordPerPage < 1 {
-			recordPerPage = 10
+			recordPerPage = 20000
 		}
 		page, err1 := strconv.Atoi(c.Query("page"))
 		if err1 != nil || page < 1 {
